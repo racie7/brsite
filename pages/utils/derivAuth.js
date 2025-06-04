e386015a70edee68e516c9fac4edcffc1ce16137
racie7 <graciamuts@gmail.com>
2025-06-04 18:45:14 +0300

    Removed stray derivAuth.js from pages/utils

--- pages/utils/derivAuth.js
+++
@@ -1,6 +0,0 @@
-export const loginWithDeriv = () => {
-  const appId = process.env.NEXT_PUBLIC_DERIV_APP_ID;
-  const redirectUri = process.env.NEXT_PUBLIC_DERIV_REDIRECT_URI;
-  const url = `https://oauth.deriv.com/oauth2/authorize?app_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-  window.location.href = url;
-};
